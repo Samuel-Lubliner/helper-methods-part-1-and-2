eb378330070a80325a507f6ad1e216898726f5f8
Samuel Lubliner <74507133+Samuel-Lubliner@users.noreply.github.com>
2023-10-17 21:28:08 +0000

    modernized controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,74 +1,53 @@
 class MoviesController < ApplicationController
   def new
-    @the_movie = Movie.new
-
-    render template: "movies/new"
+    @movie = Movie.new
   end
 
   def index
-    matching_movies = Movie.all
-
-    @list_of_movies = matching_movies.order({ :created_at => :desc })
-
+    @movies = Movie.order(created_at: :desc)
     respond_to do |format|
-      format.json do
-        render json: @list_of_movies
-      end
-
-      format.html do
-        render({ :template => "movies/index" })
-      end
+      format.json { render json: @movies }
+      format.html
     end
   end
 
   def show
-    @the_movie = Movie.find(params.fetch(:id))
+    @movie = Movie.find(params.fetch(:id))
   end
 
   def create
-    @the_movie = Movie.new
-    @the_movie.title = params.fetch("query_title")
-    @the_movie.description = params.fetch("query_description")
-
-    if @the_movie.valid?
-      @the_movie.save
-      redirect_to(movies_path, { :notice => "Movie created successfully." })
+    @movie = Movie.new
+    @movie.title = params.fetch(:title)
+    @movie.description = params.fetch(:description)
+    
+    if @movie.valid?
+      @movie.save
+      redirect_to movies_path, notice: "Movie created successfully."
     else
-      render template: "movies/new"
+      render template: "movies/new.html.erb"
     end
   end
 
   def edit
-    the_id = params.fetch(:id)
-
-    matching_movies = Movie.where(id: the_id)
-
-    @the_movie = matching_movies.first
-
-    render({ :template => "movies/edit" })
+    @movie = Movie.find(params.fetch(:id))
   end
 
   def update
-    the_id = params.fetch(:id)
-    the_movie = Movie.where({ :id => the_id }).first
+    @movie = Movie.find(params.fetch(:id))
+    @movie.title = params.fetch(:title)
+    @movie.description = params.fetch(:description)
 
-    the_movie.title = params.fetch("query_title")
-    the_movie.description = params.fetch("query_description")
-
-    if the_movie.valid?
-      the_movie.save
-      redirect_to(movie_url(the_movie), { :notice => "Movie updated successfully."} )
+    if @movie.valid?
+      @movie.save
+      redirect_to movie_url(@movie), notice: "Movie updated successfully."
     else
-      redirect_to(movie_url(the_movie), { :alert => "Movie failed to update successfully." })
+      redirect_to movie_url(@movie), alert: "Movie failed to update successfully."
     end
   end
 
   def destroy
-    the_id = params.fetch(:id)
-    the_movie = Movie.where({ :id => the_id }).first
-
-    the_movie.destroy
-
-    redirect_to(movies_url, { :notice => "Movie deleted successfully."} )
+    @movie = Movie.find(params.fetch(:id))
+    @movie.destroy
+    redirect_to movies_path, notice: "Movie deleted successfully."
   end
 end
