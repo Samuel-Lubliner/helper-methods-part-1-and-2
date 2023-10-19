9eabe435051c936f38c44eb47db041a5e4c57c16
Samuel Lubliner <74507133+Samuel-Lubliner@users.noreply.github.com>
2023-10-19 15:58:08 +0000

    refactored routes

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,8 +5,10 @@ class MoviesController < ApplicationController
 
   def index
     @movies = Movie.order(created_at: :desc)
+
     respond_to do |format|
       format.json { render json: @movies }
+
       format.html
     end
   end
@@ -15,16 +17,17 @@ class MoviesController < ApplicationController
     @movie = Movie.find(params.fetch(:id))
   end
 
-  
   def create
-    movie_attributes = params.require(:movie).permit(:title, :description)
-    @movie = Movie.new(movie_attributes)
+    movie_params = params.require(:movie).permit(:title, :description)
     
+    @movie = Movie.new(movie_params)
+
     if @movie.valid?
       @movie.save
-      redirect_to movies_path, notice: "Movie created successfully."
+
+      redirect_to movies_url, notice: "Movie created successfully."
     else
-      render template: "movies/new.html.erb"
+      render "new"
     end
   end
 
@@ -34,20 +37,21 @@ class MoviesController < ApplicationController
 
   def update
     @movie = Movie.find(params.fetch(:id))
-    @movie.title = params.fetch(:title)
-    @movie.description = params.fetch(:description)
 
-    if @movie.valid?
-      @movie.save
-      redirect_to movie_url(@movie), notice: "Movie updated successfully."
+    movie_params = params.require(:movie).permit(:title, :description)
+    
+    if @movie.update(movie_params)
+      redirect_to @movie, notice: "Movie updated successfully."
     else
-      redirect_to movie_url(@movie), alert: "Movie failed to update successfully."
+      render "edit"
     end
   end
 
   def destroy
     @movie = Movie.find(params.fetch(:id))
+
     @movie.destroy
-    redirect_to movies_path, notice: "Movie deleted successfully."
+
+    redirect_to movies_url, notice: "Movie deleted successfully."
   end
 end
